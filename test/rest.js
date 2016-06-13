71e22f64610b24ded62eec9cc5fa5fa71ea48a62
Seth Trowbridge <seth111@gmail.com>
2016-06-13 07:52:41 -0400

    modified tests for REST API

---
+++ test/rest.js
@@ -0,0 +1,39 @@
+var chai = require('chai');
+var chaiHTTP = require('chai-http');
+
+chai.should();
+chai.use(chaiHTTP);
+
+describe("REST API", function(){
+
+    describe("Create", function(){
+
+        it("Should create a project on POST", function(){
+
+        }); 
+
+    });
+
+    describe("Update", function(){
+
+        it("Should save the project on UPDATE", function(){
+
+        });
+
+    });
+
+    describe("Delete", function(){
+
+        it("Should save the project on UPDATE", function(){
+
+        });
+
+    });
+
+    describe("Read", function(){
+        it("Should load the project on GET", function(){
+
+        });
+    });
+
+});
